c93dfeed291ac579978a86fd5239d8f1e45ab34d
RhuanYuri <rhuanyuri12@gmail.com>
2025-02-18 23:28:30 -0300

    add project

---
+++ web/src/app/invite/ranking.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import cooper from '../../assets/medal-cooper.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
+
+export function Ranking() {
+  return (
+    <div className="w-full max-w-[440px] space-y-5">
+      <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+        Ranking de indicações
+      </h2>
+
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">1º</span> | Rhuã Yuri
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={gold}
+            alt="medal-gold"
+            className="absolute top-0 right-8"
+            width={56}
+            height={86}
+          />
+        </div>
+
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">2º</span> | Rhuã Yuri
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={silver}
+            alt="medal-gold"
+            className="absolute top-0 right-8"
+            width={56}
+            height={86}
+          />
+        </div>
+
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">3º</span> | Rhuã Yuri
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image
+            src={cooper}
+            alt="medal-gold"
+            className="absolute top-0 right-8"
+            width={56}
+            height={86}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
